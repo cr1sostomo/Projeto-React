e7583a908cebd5d11d2ce0343b51808b1bb53acd
cr1sostomo <lucascrisostomo9120@gmail.com>
2024-12-11 20:37:00 -0300

    11/12/2024

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+import './App.css'
+import Nav from './Nav'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Nav/>
+      <main className="App-content">
+        <span>Empty</span>
+      </main>
+
+    </div>
+
+  )
+}
+
+export default App
