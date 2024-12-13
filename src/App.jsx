092751065467a633020530b3fa731cf8d71fafc9
cr1sostomo <lucascrisostomo9120@gmail.com>
2024-12-12 21:40:36 -0300

    12.12.2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import react from 'react'
+import React, { useState } from 'react';
 import './App.css'
 import Nav from './Nav'
 
 const App = () => {
 
+  const [activeTab, setActiveTab] = useState('items');
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
       <main className="App-content">
+        <Content tab={activeTab} />
         <span>Empty</span>
       </main>
 
     </div>
 
   )
+}
+
+const Content = ({ tab }) => {
+  switch (tab) {
+    case 'items':
+      return <span>the items</span>;
+    case 'cart':
+      return <span>the cart</span>;
+    default:
+      break;
+  }
 }
 
 export default App
